b5716dd25b28cd9a9f48461887bcfe1a4d50226e
Michael berkowitz <michael.berkowitz@gmail.com>
2024-10-10 12:14:06 -0400

    Using Gamma Correction for Accurate Color Intensity

--- pkg/tracer/color.go
+++ pkg/tracer/color.go
@@ -1,14 +1,24 @@
 package tracer
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type Color = Vec3
 
 func (c Color) ToPpm() string {
 	intensity := Interval{Min: 0.000, Max: 0.999}
-	r := int(256 * intensity.Clamp(c[0]))
-	g := int(256 * intensity.Clamp(c[1]))
-	b := int(256 * intensity.Clamp(c[2]))
+	r := int(256 * intensity.Clamp(linearToGamma(c[0])))
+	g := int(256 * intensity.Clamp(linearToGamma(c[1])))
+	b := int(256 * intensity.Clamp(linearToGamma(c[2])))
 
 	return fmt.Sprintf("%d %d %d", r, g, b)
+}
+
+func linearToGamma(linear float64) float64 {
+	if linear > 0.0 {
+		return math.Sqrt(linear)
+	}
+	return 0
 }
