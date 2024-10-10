822ea5d1d5e76964b764739b7b8f36b9d4733d43
Michael berkowitz <michael.berkowitz@gmail.com>
2024-10-10 11:14:53 -0400

    Some Random Number Utilities

--- pkg/tracer/utils.go
+++ pkg/tracer/utils.go
@@ -1,7 +1,14 @@
 package tracer
 
-import "math"
+import (
+	"math"
+	"math/rand/v2"
+)
 
 func DegreesToRadians(degrees float64) float64 {
 	return degrees * math.Pi / 180.0
+}
+
+func RandomIn(min, max float64) float64 {
+	return min + (max-min)*rand.Float64()
 }
